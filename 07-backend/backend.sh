db7744fbd02d388726a748791344cae4b4381546
medasani <medasanicharan66@gmail.com>
2025-04-30 18:17:13 +0530

    01-backend-deployment

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-app_Version=$1
+component=$1
+environment=$2
+app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/MEDASANI-CHARAN/03-expense-ansible-roles.git backend.yaml -e appVersion=$app_Version
+ansible-pull -i localhost, -U https://github.com/MEDASANI-CHARAN/04-expense-ansible-roles.tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
